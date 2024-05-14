d18b5d7b606bb3b7e49b99d1670835a87261ed38
Troy Rice <troyanthonyrice@gmail.com>
2024-05-14 14:55:44 -0700

    GET by id tests passing

--- api/server.js
+++ api/server.js
@@ -22,7 +22,14 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => {
-            res.json(user)
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            } else {
+                res.json(user)
+            }
+
         })
         .catch(err => {
             res.status(500).json({
