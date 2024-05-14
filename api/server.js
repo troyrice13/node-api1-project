d981fbfad7d3b7497630c3a40372915a2f0c2758
Troy Rice <troyanthonyrice@gmail.com>
2024-05-14 14:39:56 -0700

    GET passing

--- api/server.js
+++ api/server.js
@@ -2,10 +2,22 @@
 
 const express = require('express');
 
+const User = require('./users/model')
+
 const server = express();
 
 server.get('/api/users', (req, res) => {
-    res.json('users')
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack
+            })
+        })
 })
 
 
